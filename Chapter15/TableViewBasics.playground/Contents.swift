b10a39fb5a01e0394d2166a67149bd62ed27c5bb
Ahmad Sahar <shah@mac.com>
2020-07-28 17:48:15 +0800

    chap 10 - 20

---
+++ Chapter15/TableViewBasics.playground/Contents.swift
@@ -0,0 +1,40 @@
+import UIKit
+
+import PlaygroundSupport
+
+class TableViewExampleController: UIViewController, UITableViewDataSource {
+    
+    var tableView: UITableView?
+    var names:[String] = ["Jane", "Akhil", "Divij"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.bounds = CGRect(x: 0, y: 0, width: 375, height: 667)
+        createTableView()
+    }
+    
+    func createTableView() {
+        self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        self.tableView?.dataSource = self
+        self.tableView?.backgroundColor = .white
+        self.tableView?.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        self.view.addSubview(self.tableView!)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let name = names[indexPath.row]
+        cell.textLabel?.text = name
+        return cell
+    }
+    
+
+}
+
+PlaygroundPage.current.liveView = TableViewExampleController()
+
