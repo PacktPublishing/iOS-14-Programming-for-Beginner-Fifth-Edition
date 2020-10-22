636899e58ca8f21eac6d0ab2b0d113ef4b76bc93
deepesh-packt <deepeshp@packt.com>
2020-10-22 14:44:29 +0530

    iOS14

---
+++ Chapter10/Chapter10/LetsEat/LetsEat/Explore/ExploreViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ExploreViewController.swift
+//  LetsEat
+//
+//  Created by iOS 14 Programming on 02/10/2020.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "header", for: indexPath)
+        return headerView
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "exploreCell", for: indexPath)
+        return cell
+    }
+    
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindLocationCancel(segue:UIStoryboardSegue){
+        
+    }
+}
